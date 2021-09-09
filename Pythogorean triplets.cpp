1528f5e9157b0942dadaac14f5a119614390b2a7
Hackr <81916900+123452123-codr@users.noreply.github.com>
2021-09-09 09:39:33 +0530

    Update Pythogorean triplets.cpp

--- Pythogorean triplets.cpp
+++ Pythogorean triplets.cpp
@@ -2,29 +2,38 @@
 #include<conio.h>
 void main()
 {
-  clrscr();
-  long int a,b,c;
-  cout<<"Enter the value of three sides:";
-  cin>>a>>b>>c;
-  if(a >= b+c || b >= a+c || c >= a+b)
-  {
-    cout<<"Error! Invalid input!";
+    clrscr();
+    long int a,b,c;
+    cout<<"Enter the value of three sides:";
+    cin>>a>>b>>c;
+    if(a >= b+c || b >= a+c || c >= a+b)
+    {
+        cout<<"Error! Invalid input!";
+    }
     else
     {
-      if(a>b && a>c)
-      {
-        if(a*a == b*b+c*c)
-          cout<<"Yes";
-        else
-          cout<<"No";
-      }
-      else if(b>a && b>c)
-      {
-        if(a*a == b*b+c*c)
-          cout<<"Yes";
-        else
-          cout<<"No";
-      }
+        if(a>b && a>c)
+        {
+            if(a*a == b*b+c*c)
+            {
+                cout<<"Yes";
+            }
+            else
+            {
+                cout<<"No";
+            }
+        }
+        else if(b>a && b>c)
+        {
+            if(a*a == b*b+c*c)
+            {
+                cout<<"Yes";
+            }
+            else
+            {
+                cout<<"No";
+            }
+        }
       else
       {
         if(a*a == b*b+c*c)
